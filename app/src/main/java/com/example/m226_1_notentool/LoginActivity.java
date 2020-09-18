a8e2696110e874cae2536d0531eb9a48201f046a
Natiq <natiqkhan00@gmail.com>
2020-09-18 14:40:17 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/m226_1_notentool/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.m226_1_notentool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClick(View v) {
+        signIn();
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+}
