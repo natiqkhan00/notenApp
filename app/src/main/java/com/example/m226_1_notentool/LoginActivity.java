76c383d9f515a97d30a5fdb7f4f611291e52995d
Natiq <natiqkhan00@gmail.com>
2020-10-23 14:58:24 +0200

    Added Google Auth SSO

--- app/src/main/java/com/example/m226_1_notentool/LoginActivity.java
+++ app/src/main/java/com/example/m226_1_notentool/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -27,17 +28,17 @@ public class LoginActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     private final static int RC_SIGN_IN = 123;
     private FirebaseAuth mAuth;
+    private static final String TAG = "GoogleActivity";
 
     @Override
     protected void onStart() {
         super.onStart();
 
         FirebaseUser user = mAuth.getCurrentUser();
-        if(user!=null){
+        if(user != null){
             Intent intent = new Intent(getApplicationContext(),Profile.class);
             startActivity(intent);
         }
-
     }
 
     @Override
@@ -62,6 +63,7 @@ public class LoginActivity extends AppCompatActivity {
     private void createRequest() {
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -86,7 +88,9 @@ public class LoginActivity extends AppCompatActivity {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(this, e.getMessage() , Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -99,7 +103,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Intent intent = new Intent(getApplicationContext(),Profile.class);
+                            Intent intent = new Intent(getApplicationContext(), Profile.class);
                             startActivity(intent);
                         } else {
                             // If sign in fails, display a message to the user.
